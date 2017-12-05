30038135d0e2a4fe0029fcae23c7cca689c866a8
Ryan Machan <rjm21@case.edu>
2017-12-05 15:41:26 -0500

    begin sign-in / sign-out logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var firebaseConfig = {
+	apiKey: 'AIzaSyDs9iz9p-7I_mGuLqHUlhnz-sSQyqwgE_c',
+	authDomain: 'for-the-tour.firebaseapp.com',
+	databaseURL: 'https://for-the-tour.firebaseio.com',
+	projectId: 'for-the-tour',
+	storageBucket: 'for-the-tour.appspot.com',
+	messagingSenderId: '725149234874'
+};
+firebase.initializeApp(firebaseConfig);
+
+$(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip();
+});
