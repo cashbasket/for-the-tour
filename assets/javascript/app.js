8dabd401fe7dd5e2a45e198dbcb29ef6d9368886
Ryan Machan <rjm21@case.edu>
2017-12-06 01:34:31 -0500

    add events table functionality and basic styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,11 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 $(document).ready(function() {
-	$('[rel="tooltip"]').tooltip();
+	$('body').tooltip({
+		selector: '[rel=tooltip]'
+	});
+
+	$('.results-table-wrapper').hide();
 
 	$('#searchForm').on('submit', function(event) {
 		event.preventDefault();
@@ -23,15 +27,47 @@ $(document).ready(function() {
 					console.log(response);
 					for (var i = 0; i < response.length; i++) {
 						var curEvent = response[i];
-						var tr = $('<tr><td>' + band + '</td><td>' + curEvent.datetime + '</td><td>' + curEvent.venue.name + '</td><td>' + curEvent.venue.city + ', ' + curEvent.venue.region + '</td><td>' + curEvent.venue.country + '</td>');
-						$('#resultsTable > tbody').append(tr);
+						var dateFormat = moment(curEvent.datetime).format('M/D/YYYY @ h:mma');
+						var lineup = '';
+						var location = curEvent.venue.city;
+						if (curEvent.venue.region.length) {
+							location += ', ' + curEvent.venue.region;
+						}
+						for (var j=0; j < curEvent.lineup.length; j++) {
+							lineup += curEvent.lineup[j];
+							if (j < curEvent.lineup.length - 1) {
+								lineup += ', ';
+							}
+						}
+						
+						var tr = $('<tr><td width="300">' + lineup + '</td><td>' + dateFormat  + '</td><td>' + curEvent.venue.name + '</td><td>' + location + '</td><td>' + curEvent.venue.country + '</td>');
+						var rsvpCell = $('<td>');
+						var rsvpButton = $('<button>');
+						rsvpButton.addClass('rsvp btn btn-rsvp');
+						rsvpButton.attr('data-artist', band)
+							.attr('data-date', curEvent.datetime)
+							.attr('data-venue', curEvent.venue.name)
+							.attr('data-city', curEvent.venue.city)
+							.attr('data-region', curEvent.venue.region)
+							.attr('data-country', curEvent.venue.country)
+							.attr('data-target', '#rsvpModal')
+							.text('RSVP');
+						
+						// if user is not logged in, add the tooltip stuff
+						if($('.user-info').text().length === 0) {
+							rsvpButton.attr('data-placement', 'top')
+								.attr('rel', 'tooltip')
+								.attr('title', 'You must be signed in to RSVP');
+						}
+							
+						$('#resultsTable > tbody').append(tr.append(rsvpCell.append(rsvpButton)));
+						$('.results-table-wrapper').hide().slideDown();
 					}
 				})
 				.fail(function (error) {
 					console.log(error);
 				});
-		} else {
-			$('#bandName').val('');
-		}
+		} 
+		$('#bandName').val('');
 	});
 });
