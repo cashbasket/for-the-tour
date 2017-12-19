c0429ee9e39289776474194c4198977f45a06811
Ryan Machan <rjm21@case.edu>
2017-12-18 21:00:29 -0500

    update navbar so search input is always visible

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -366,28 +366,6 @@ $(document).ready(function() {
 	} 
 	$('#bandName, #subBandName').val('');
 
-	$('body, .navbar-collapse form[role="search"] button[type="reset"]').on('click keyup', function(event) {
-		if (event.which == 27 && $('.navbar-collapse form[role="search"]').hasClass('active') ||
-			$(event.currentTarget).attr('type') == 'reset') {
-			closeSearch();
-		}
-	});
-
-	function closeSearch() {
-		var $form = $('.navbar-collapse form[role="search"].active')
-		$form.find('input').val('');
-		$form.removeClass('active');
-	}
-
-	// Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
-	$(document).on('click', '.navbar-collapse form[role="search"]:not(.active) button[type="submit"]', function(event) {
-		event.preventDefault();
-		var $form = $(this).closest('form'),
-			$input = $form.find('input');
-		$form.addClass('active');
-		$input.focus();
-	});
-
 	$('body').on('click', '.rsvp', function() {
 		$('.rsvp-warning, #rsvpForm').addClass('hidden');
 		$('#venueLoading, #addToCalendarLink').removeClass('hidden');
--- assets/javascript/view-rsvps.js
+++ assets/javascript/view-rsvps.js
@@ -157,28 +157,6 @@ $(document).ready(function() {
 	$('body').tooltip({
 		selector: '[data-toggle=tooltip]'
 	});
-	
-	$('body, .navbar-collapse form[role="search"] button[type="reset"]').on('click keyup', function(event) {
-		if (event.which == 27 && $('.navbar-collapse form[role="search"]').hasClass('active') ||
-			$(event.currentTarget).attr('type') == 'reset') {
-			closeSearch();
-		}
-	});
-
-	function closeSearch() {
-		var $form = $('.navbar-collapse form[role="search"].active')
-		$form.find('input').val('');
-		$form.removeClass('active');
-	}
-
-	// Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
-	$(document).on('click', '.navbar-collapse form[role="search"]:not(.active) button[type="submit"]', function(event) {
-		event.preventDefault();
-		var $form = $(this).closest('form'),
-			$input = $form.find('input');
-		$form.addClass('active');
-		$input.focus();
-	});
 
 	if(eventId) {
 		getEventInfo(eventId);
